83b9616d6cce8c7be3dc6a0eedba5670e6221e80
Vivek1601 <vivekkhandelwal1609@gmail.com>
2024-02-12 23:43:23 +0530

    first commit

--- data/database.js
+++ data/database.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 export const connectDb = ()=>{mongoose.connect(process.env.MONGO_URL,{
     dbName : "Backend_API"
-}).then(()=>console.log("Database connected")).catch((e) => {
+}).then((c)=>console.log(`Database connected with ${c.connection.host}`)).catch((e) => {
     console.log(e);
 })
 }
